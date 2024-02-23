eb4c3806e78e81b85a382c89e002ab166cc702ec
hdutoit <hdutoit@ineat.fr>
2024-02-23 15:14:09 +0100

    feat(prestataire): - prestataire card added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Box, Button, Container, Typography } from '@mui/material';
+import PrestataireCard, { Prestataire } from './components/PrestataireCard';
+import AddIcon from '@mui/icons-material/Add';
 function App() {
+  const [prestataires, setPrestataires] = useState<Prestataire[]>()
+  useEffect(() => {
+    setPrestataires(
+      [
+        {
+          name: "plop",
+          nbLike: 10000,
+          url: "https://www.francetvinfo.fr/pictures/zwzFc6PU5JuzL2NA0BwMqhCrvo0/1200x900/2016/08/23/shrek-5.jpg"
+        }
+      ]
+    )
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      {prestataires?.map((prestataire) => (
+        <PrestataireCard
+          {...prestataire}
+        />
+      ))}
+      <Button> <AddIcon></AddIcon> add prestataire</Button>
+    </Container>
   );
 }
 
---
+++ src/components/PrestataireCard.tsx
@@ -0,0 +1,35 @@
+import { Avatar, Card, CardContent, CardHeader, IconButton } from "@mui/material"
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+export type Prestataire = {
+    name: string,
+    url: string,
+    nbLike: number, 
+}
+export default ({name, url, nbLike}:Prestataire) => {
+    return (
+        <>
+            <Card variant="outlined">
+                <CardHeader
+                    avatar={
+                        <Avatar alt="Remy Sharp" src={url} />
+                    }
+                    action={
+                        <>
+                            <IconButton aria-label="settings">
+                                {
+                                    false 
+                                    ?<FavoriteIcon/>
+                                    :<FavoriteBorderIcon/>
+                                }
+                            </IconButton>
+                            {nbLike.toLocaleString('en-US', { style: 'decimal', maximumFractionDigits : 0, minimumFractionDigits: 0, useGrouping: true })}
+                        </>
+                    }
+                    title={name}
+                />
+            </Card>
+        </>
+    )
+}
